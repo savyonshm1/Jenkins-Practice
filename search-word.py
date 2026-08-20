3e56f2dfabcbe6933766efbf45292c0d830ebd94
savyonshm1 <savyonshaham@gmail.com>
2026-08-20 21:04:04 +0300

    v3

---
+++ search-word.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+# check whether agg exists
+if len(sys.argv) != 2:
+
+# check if the argument is a file 
+
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+
+    
